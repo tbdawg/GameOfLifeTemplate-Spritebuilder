aadb11838d6f4be76f4f926805a2f04f69689c92
ChaoticDawg <ChaoticDawgSoftware@hotmail.com>
2014-10-22 18:42:01 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -82,4 +82,87 @@ static const int GRID_COLUMNS = 10;
     return _gridArray[row][column];
 }
 
+-(void) evolveStep {
+    // update each creature's neighbor count
+    [self countNeighbors];
+    
+    // update each creature's state
+    [self updateCreatures];
+    
+    // update the generation so the label's text will display the correct generation
+    _generation++;
+    
+}
+
+-(void) countNeighbors {
+    // iterate through the rows
+    // note that NSArray has a method 'count' that will return the number of elements
+    
+    for (int i = 0; i < [_gridArray count]; i++) {
+        
+        // iterate through all the columns for a given row
+        for (int j = 0; j < [_gridArray[i] count]; j++) {
+            
+            // access the creature in the cell that corresponds to the current row/column
+            Creature *currentCreature = _gridArray[i][j];
+            
+            // remember that every creature has a livingNeighbors property that we created earlier
+            currentCreature.livingNeighbors = 0;
+            
+            // now examine every cell around the current one
+            
+            // go through the row on the top of the current cell, the row the cell is in,
+            // and the row below the current cell
+            for (int x = (i-1); x <= (i+1); x++) {
+                
+                // go through the column to the left of the current cell, the column the current cell
+                // is in and the column to the right of the current cell.
+                for (int y = (j-1); y <= (j+1); y++) {
+                    
+                    // check that the cell we're checking isn't off the screen
+                    BOOL isIndexValid = [self isIndexValidForX:x andY:y];
+                    
+                    // skip over all cells that are off the screen AND the cell that contains the
+                    // creature we are currently updating.
+                    if (!((x == i) && (y == j)) && isIndexValid) {
+                        Creature *neighbor = _gridArray[x][y];
+                        if (neighbor.isAlive) {
+                            currentCreature.livingNeighbors += 1;
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+    
+}
+
+-(void) updateCreatures {
+    for (int row = 0; row < [_gridArray count]; row++) {
+        
+        for (int col = 0; col < [_gridArray[row] count]; col++) {
+            
+            Creature *currentCreature = _gridArray[row][col];
+            
+            if (currentCreature.livingNeighbors == 3) {
+                currentCreature.isAlive = YES;
+            } else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
+                currentCreature.isAlive = NO;
+            }
+        }
+    }
+    
+}
+
+-(BOOL)isIndexValidForX:(int)x andY:(int)y {
+    BOOL isIndexValid = YES;
+    if(x < 0 || x >= GRID_ROWS || y >= GRID_COLUMNS) {
+        isIndexValid = NO;
+    }
+    
+    return isIndexValid;
+}
+
+
 @end
